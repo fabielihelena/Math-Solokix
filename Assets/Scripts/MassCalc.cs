c3ceac09564a3a47791aceca62a2760473d53efd
zappknight <victorfernandes.nave@gmail.com>
2015-06-02 22:09:32 -0300

    Câmera e calculos Fisicos
    Foram adicionados a câmera orbital e alguns cálculos de massa.

---
+++ Assets/Scripts/MassCalc.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MassCalc : MonoBehaviour {
+	
+	void Start () {
+		if(name.Equals("Cube"))
+		{
+			rigidbody.mass = CubicMass(transform.localScale,"");
+		}
+		else if(name.Equals("Cylinder"))
+		{
+			rigidbody.mass = CylinderMass(transform.localScale,"");
+		}
+	}
+
+	float CubicMass(Vector3 Scale, string material)
+	{
+		switch (material) 
+		{
+			case "ferro":
+				return Scale.x*Scale.y*Scale.z*3;
+				break;
+			default:
+				return Scale.x*Scale.y*Scale.z*1;
+				break;
+		}
+	}
+
+
+	float CylinderMass(Vector3 Scale, string material)
+	{
+		switch (material) 
+		{
+			case "ferro":
+				return (Mathf.PI*Scale.x*Scale.y*Scale.z)*3;
+				break;
+			default:
+				return (Mathf.PI*Scale.x*Scale.y*Scale.z)*1;
+				break;
+		}
+	}
+}

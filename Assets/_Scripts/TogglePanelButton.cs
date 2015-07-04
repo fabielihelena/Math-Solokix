9d83c492150e0ef46468d715450d716cdeb7405b
yagoazedias <yagoluis9@hotmail.com>
2015-07-04 12:38:37 -0300

    Version 0.6
    Paineis 80 % ajustados

--- Assets/_Scripts/TogglePanelButton.cs
+++ Assets/_Scripts/TogglePanelButton.cs
@@ -8,4 +8,9 @@ public class TogglePanelButton : MonoBehaviour {
 
     public void CreateCube(GameObject panel)
     {  }
+
+    public void ActivePanel (GameObject painel)
+    { painel.SetActive(true); }
+    public void DesablePanel (GameObject painel)
+    { painel.SetActive(false); }
 }

3aba3c5decf7caa4d556083fe892782f9caadd9d
yagoazedias <yagoluis9@hotmail.com>
2015-07-04 00:17:48 -0300

    Version 0.5
    4 balanças ja criadas, cada uma com um resultado no minimo

---
+++ Assets/_Scripts/Main.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Main : MonoBehaviour {
+
+    public List<GameObject> btList = new List<GameObject>();
+
+	void Start () 
+    {
+        //Time.timeScale = 0f;
+	}
+
+	void FixedUpdate ()
+    {
+        //Time.timeScale = 0f;
+	}
+}
---
+++ Assets/_Scripts/TogglePanelButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TogglePanelButton : MonoBehaviour {
+
+	public void TogglePanel (GameObject panel)
+	{ panel.SetActive(!panel.activeSelf); }
+
+    public void CreateCube(GameObject panel)
+    {  }
+}

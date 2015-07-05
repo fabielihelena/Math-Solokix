07a17c53bd4056478936357d720a14e6addd1396
zappknight <victorfernandes.nave@gmail.com>
2015-07-04 21:49:09 -0300

    Versão 0.73
    Adicionamos um teste de instanciamento coordenado.

--- Assets/_Scripts/Main.cs
+++ Assets/_Scripts/Main.cs
@@ -12,17 +12,30 @@ public class Main : MonoBehaviour {
     public Texture[] textures;
     public float actualHigh;
     public float actualSize;
+    public float totalQuantity = 0;
     
-    public void SpawnObject(Vector3 startPosition)
+    public void SpawnObject()
     {
-        if(actualPoligon.Equals("Cube"))
-        {
-            GameObject obj = shapes[0];
-            obj.SetActive(true);
-            obj.tag = actualMaterial;
-            obj.AddComponent<MassCalc>();
-            obj.GetComponent<Rigidbody>().Sleep();
-        }
+        Vector3 t = actualPuzzle.transform.FindChild("InstantiateSPoint").position;
+        int g =actualQuantity;
+            for (int i = 0; i < 3; i++)
+            {
+                if (actualPoligon.Equals("Cubo"))
+                {
+                    Debug.Log("oioio");
+                    GameObject obj = shapes[0];
+                    obj.SetActive(true);
+                    obj.tag = actualMaterial;
+                    obj.AddComponent<MassCalc>();
+                    obj.GetComponent<Rigidbody>().useGravity = false;
+                    obj.GetComponent<BoxCollider>().enabled = false;
+                    obj.transform.localScale = new Vector3(actualSize, actualHigh, actualSize);
+                    obj.transform.position = new Vector3(t.x, t.y, t.z - (actualSize * i));
+                    Instantiate(obj);
+                    g--;
+                }
+                if (g >= 0) break;
+            }
     }
 
 	void Start ()
